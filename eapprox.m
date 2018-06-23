c04e5d629ed98e6bda09ecdafebb37cb973165d7
Atreya Shankar <shankar.atreya@gmail.com>
2018-06-23 11:54:20 +0200

    updates

--- eapprox.m
+++ eapprox.m
@@ -1,17 +1,19 @@
-N = 1000000;
+N = 1000;
 s = 0;
 k = 0;
 f = 0;
+g = 0;
 
 for i=1:N
   while s <= 1
-    s = s + rand;
-    k = k + 1;
-   end
-f(i) = k;
-s = 0;
-k = 0;
+   s = s + rand;
+   k = k + 1;
+  end
+  f(i) = k;
+  g(i) = mean(f(1:i));
+  s = 0;
+  k = 0;
 end
 
-# disp(f)
-disp(mean(f))
+disp(g(N));
+plot(g);
